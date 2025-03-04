b0eada0459d82afbf9a6a26b1c4d2f1474fb804d
NenadN22 <tonbe22@gmail.com>
2025-03-04 22:22:39 +0100

    Create event listener for divs so divs have colors, also add new function so after user click on change size can use prompt to choose from 0 to 100

--- script.js
+++ script.js
@@ -2,12 +2,33 @@ const container = document.querySelector('.container')
 // Create function 
 function createGrid(rows) {
     for(let i = 0; i < rows * rows; i++) {
-        const gridDiv = document.createElement('div');
+       const gridDiv = document.createElement('div')
         gridDiv.classList.add('grid-div');
         container.appendChild(gridDiv)
-        gridDiv.style.width = `${container.offsetWidth / rows}px`;
-        gridDiv.style.height = `${container.offsetHeight / rows}px`;
+        gridDiv.style.width = `${container.clientWidth  / rows }px `;
+        gridDiv.style.height = `${container.clientHeight / rows }px`;
+        gridDiv.addEventListener('mouseover', () => {
+            gridDiv.style.backgroundColor  = 'black';
+        })
+        
+        
     }
-
 }
+function buttonPrompt(){
+    const promptButton = document.querySelector('button')
+    let choice = '';
+    promptButton.addEventListener('click', () => {
+            choice =  prompt('How many grid you want from 0 to 100?')
+            if(choice > 100) {
+                choice = prompt('Not available choose from 0 to  100?')
+            } 
+            console.log(choice)
+            container.innerHTML = '';
+            createGrid(choice)
+    })
+    
+}
+
+
 createGrid(16)
+buttonPrompt()
